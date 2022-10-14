8ea1087d42761cca9c0828dfaba923cec12db826
Musthafa Faishal <musthafafaishal@gmail.com>
2022-10-14 10:50:15 +0700

    update router

---
+++ router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"go-jwt/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartApp() *gin.Engine {
+	r := gin.Default()
+
+	userRouter := r.Group("/users")
+	{
+		userRouter.POST("/register", controllers.UserRegister)
+	}
+
+	return r
+}
