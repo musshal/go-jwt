90babb019e1f218ef98ffb37222a25e42191701d
Musthafa Faishal <musthafafaishal@gmail.com>
2022-10-14 10:18:06 +0700

    update models

---
+++ models/user.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"github.com/asaskevich/govalidator"
+	"github.com/gorm"
+)
+
+type User struct {
+	GormModel
+	FullName string    `gorm:"not null" json:"full_name" form:"full_name" valid:"required-Your full name is required"`
+	Email    string    `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required-Your email is required,email-invalid email format"`
+	Password string    `gorm:"not null" json:"password" form:"password" valid:"required-Your password is required,minstringlenght(6)-Password has to have a minimum length of 6 characters"`
+	Products []Product `gorm:"constraint:onUpdate:CASCADE,onDelete:SET NULL" json:"products"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	_, errCreate := govalidator.ValidateStruct(u)
+
+	if errCreate != nil {
+		err = errCreate
+
+		return
+	}
+
+	err = nil
+
+	return
+}
