b7e4d97ab37840944fa5605bb75f9f285c07e542
Musthafa Faishal <musthafafaishal@gmail.com>
2022-10-14 10:46:14 +0700

    update controllers

---
+++ controllers/userControllers.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"go-jwt/database"
+	"go-jwt/helpers"
+	"go-jwt/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	appJSON = "application/json"
+)
+
+func UserRegister(c *gin.Context) {
+	db := database.GetDB()
+	contentType := helpers.GetContentType(c)
+	_, _ = db, contentType
+	User := models.User{}
+
+	if contentType == appJSON {
+		c.ShouldBindJSON(&User)
+	} else {
+		c.ShouldBind(&User)
+	}
+
+	err := db.Debug().Create(&User).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error":   "Bad Request",
+			"message": err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"id": User.ID
+		"email": User.Email
+		"full_name": User.FullName
+	})
+}
